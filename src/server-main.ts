b3a1eefce032c3ada5422c2e07acc69dfe1b959e
Jeroen Vinke <JeroenVinke17@gmail.com>
2017-12-31 11:42:33 +0100

    first commit

---
+++ src/server-main.ts
@@ -0,0 +1,18 @@
+﻿/// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
+// we want font-awesome to load as soon as possible to show the fa-spinner
+import '../static/styles.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Aurelia } from 'aurelia-framework';
+import { PLATFORM } from 'aurelia-pal';
+import bootstrapper from './ssr-bootstrapper-webpack';
+
+async function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration();
+
+  await aurelia.start();
+  await aurelia.setRoot(PLATFORM.moduleName('app'));
+}
+
+module.exports = bootstrapper(configure);
